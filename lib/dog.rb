810e0534d4f5af959d8809e5b23c2d5d2bca8206
Jon Mackey <jon@jonmackey.party>
2019-01-30 22:01:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,7 +31,11 @@ class Dog
     DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     self
-    end
+  end
+  
+  def self.create(attribute_hash)
+    
+    
 
 end
     
